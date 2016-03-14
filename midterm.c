91723f71a9c95cdb3c174fad8905b24326711a10
chilichilimo <afshar.m.95@gmail.com>
2016-03-14 17:52:44 +0000

    Added writeToFile and readFromFile

--- midterm.c
+++ midterm.c
@@ -9,6 +9,57 @@
 #include "abdrive.h"
 #include "ping.h"
 
+int DO = 22, CLK = 23, DI = 24, CS = 25;            // SD card pins on Activity Board
+
+void writeToFile(int x, int y)
+{
+    int erc = sd_mount(DO, CLK, DI, CS);              // Mount SD card
+    if(!erc)                                          // Error code = 0, good, continue
+    {
+        FILE* fp = fopen("test.txt", "w");              // Open a file for writing
+        
+        if(fp)                                          // Nonzero file pointer?
+        {                                               // Good, continue
+            char strx[] = x;
+            char stry[] = y;
+            fwrite(strx, 1, 21, fp);                    // Add contents to the file
+            fwrite(stry, 1, 21, fp);                    // Add contents to the file
+            fwrite("\n", 1, 21, fp);                    // Add contents to the file
+        }
+        else                                            // Zero file pinter?
+        {                                               // Bad, error message.
+            print("File did not open.\n");
+        }
+        
+        fclose(fp);                                     // Close the file
+    }
+    else                                              // Mount error code not zero?
+    {                                                 // Bad, display code
+        print("Error opening card.");
+        print("error code = %d\n", erc);
+    }
+}
+void readFromFile()
+{
+    fp = fopen("test.txt", "r");                    // Reopen file for reading.
+    
+    char s[80];                                     // Buffer for characters.
+    
+    if(fp)                                          // Nonzero file pinter?
+    {                                               // Good, continue.
+        print("First 21 chars in test.txt:\n");       // Display heading
+        fread(s, 1, 21, fp);                          // Read 21 characters
+        print("%s", s);                               // Display them
+        print("\n");                                  // With a newline at the end.
+    }
+    else                                            // Zero file pointer?
+    {                                               // Bad, print error.
+        print("File did not open.\n");
+        print("\n");
+    }
+    fclose(fp);
+}
+
 int irLeft, irRight;                            // IR variables
 int target = 20;
 int multiplier = 5;
@@ -96,6 +147,7 @@ int main()                                      // main function
             }
             previous->next = current;
             current->prev = previous;
+            writeToFile(current.DataLeft,current.dataRight);
             previous = current;
         }
 
